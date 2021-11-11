c92206dbd1a5b508dcf291b209832ca2ed11f40c
Martin Ligabue <martinligabue@gmail.com>
2021-11-11 15:16:14 +0100

    ctrlshifti

--- tests/mp/exercise/expressions/ExpressionEvaluatorVisitorTest.java
+++ tests/mp/exercise/expressions/ExpressionEvaluatorVisitorTest.java
@@ -8,94 +8,56 @@ public class ExpressionEvaluatorVisitorTest {
 
     @Test
     public void testEvalConstant() {
-        assertEquals(10,
-            new Constant(10)
-            .accept(new ExpressionEvaluatorVisitor()));
-        assertEquals(true,
-            new Constant(true)
-            .accept(new ExpressionEvaluatorVisitor()));
-        assertEquals("Hello",
-            new Constant("Hello")
-            .accept(new ExpressionEvaluatorVisitor()));
+        assertEquals(10, new Constant(10).accept(new ExpressionEvaluatorVisitor()));
+        assertEquals(true, new Constant(true).accept(new ExpressionEvaluatorVisitor()));
+        assertEquals("Hello", new Constant("Hello").accept(new ExpressionEvaluatorVisitor()));
     }
 
     @Test
     public void testEvalEqualsTwoConstants() {
-        assertEquals(true,
-            new Equal(
-                new Constant(10),
-                new Constant(10)
-            )
-            .accept(new ExpressionEvaluatorVisitor()));
+        assertEquals(true, new Equal(new Constant(10), new Constant(10)).accept(new ExpressionEvaluatorVisitor()));
+        assertEquals(false, new Equal(new Constant(1), new Constant(10)).accept(new ExpressionEvaluatorVisitor()));
         assertEquals(false,
-            new Equal(
-                new Constant(1),
-                new Constant(10)
-            )
-            .accept(new ExpressionEvaluatorVisitor()));
-        assertEquals(false,
-            new Equal(
-                new Constant("hello"),
-                new Constant(10)
-            )
-            .accept(new ExpressionEvaluatorVisitor()));
+                new Equal(new Constant("hello"), new Constant(10)).accept(new ExpressionEvaluatorVisitor()));
     }
 
     @Test
     public void testEvalEqualsTwoExpressions() {
-        assertEquals(true,
-            new Equal(
-                new Sum(new Constant(10), new Constant(2)),
-                new Multiplication(new Constant(6), new Constant(2))
-            )
-            .accept(new ExpressionEvaluatorVisitor()));
-        assertEquals(false,
-            new Equal(
-                new Sum(new Constant(1), new Constant(2)),
-                new Multiplication(new Constant(6), new Constant(2))
-            )
-            .accept(new ExpressionEvaluatorVisitor()));
+        assertEquals(true, new Equal(new Sum(new Constant(10), new Constant(2)),
+                new Multiplication(new Constant(6), new Constant(2))).accept(new ExpressionEvaluatorVisitor()));
+        assertEquals(false, new Equal(new Sum(new Constant(1), new Constant(2)),
+                new Multiplication(new Constant(6), new Constant(2))).accept(new ExpressionEvaluatorVisitor()));
     }
 
     @Test
     public void testEvalSumTwoIntegers() {
-        assertEquals(15,
-            new Sum(new Constant(5), new Constant(10))
-            .accept(new ExpressionEvaluatorVisitor()));
+        assertEquals(15, new Sum(new Constant(5), new Constant(10)).accept(new ExpressionEvaluatorVisitor()));
     }
 
     @Test
     public void testEvalMultiplicationTwoIntegers() {
-        assertEquals(15,
-            new Multiplication(new Constant(5), new Constant(3))
-            .accept(new ExpressionEvaluatorVisitor()));
+        assertEquals(15, new Multiplication(new Constant(5), new Constant(3)).accept(new ExpressionEvaluatorVisitor()));
     }
 
     @Test
     public void testEvalSumAndMultiplicationTwoIntegers() {
-        assertEquals(16,
-            new Multiplication(
-                new Sum(new Constant(5), new Constant(3)),
-                new Constant(2))
+        assertEquals(16, new Multiplication(new Sum(new Constant(5), new Constant(3)), new Constant(2))
             .accept(new ExpressionEvaluatorVisitor()));
     }
 
     @Test
     public void testEvalSumNonIntegers() {
         ClassCastException thrown = assertThrows(ClassCastException.class,
-            () -> new Sum(new Constant(5), new Constant("BANG!"))
-                .accept(new ExpressionEvaluatorVisitor()));
-        assertEquals("java.lang.String cannot be cast to java.lang.Integer",
-            thrown.getMessage());
+                () -> new Sum(new Constant(5), new Constant("BANG!")).accept(new ExpressionEvaluatorVisitor()));
+        assertEquals("class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap')", thrown.getMessage());
     }
 
     @Test
     public void testEvalMultiplicationNonIntegers() {
         ClassCastException thrown = assertThrows(ClassCastException.class,
-            () -> new Multiplication(new Constant(false), new Constant(3))
-                .accept(new ExpressionEvaluatorVisitor()));
-        assertEquals("java.lang.Boolean cannot be cast to java.lang.Integer",
-                thrown.getMessage());
+                () -> new Multiplication(new Constant(false), new Constant(3))
+                    .accept(new ExpressionEvaluatorVisitor()));
+        assertEquals("class java.lang.Boolean cannot be cast to class java.lang.Integer (java.lang.Boolean and java.lang.Integer are in module java.base of loader 'bootstrap')", thrown.getMessage());
     }
 
 }
